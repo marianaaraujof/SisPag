bb8e525bb89e2bdf1b4eb2d5561c528f95141fd4
Mariana Araujo <marianaaraujof@ufrn.edu.br>
2018-03-22 11:16:32 -0300

    Variaveis de sessão ajeitadas

--- controller/controlerformulario.php
+++ controller/controlerformulario.php
@@ -1,24 +1,35 @@
 <?php
   require_once('../session/session.php');
   
-  $_SESSION['_cpf'] = $_POST["cpf"];
-  $_SESSION['_Nome'] = $_POST["nomefuncionario"];
-  $_SESSION['_AnoNasc'] = $_POST["nascimento"];
-  $_SESSION['_SBase'] = $_POST["salario"];
-  $_SESSION['_NumFilhos'] = $_POST["filhos"];
+
+  $_CPF = $_POST["cpf"];
+  $_NomeFunc = $_POST["nomefuncionario"];
+  $_AnoNasc = $_POST["nascimento"];
+  $_SBase = $_POST["salario"];
+  $_NumFilhos = $_POST["filhos"];
+
 
   include '../library/library.php';
   
-  $_SESSION['_SFamilia'] = calc_familia($_SESSION['_NumFilhos']);
-  $_SESSION['_Abono'] = calc_abono($_SESSION['_AnoNasc']);
-  $_SESSION['_Idade'] = calc_idade($_SESSION['_AnoNasc']);
-  $_SESSION['_SBruto'] = calc_bruto($_SESSION['_SFamilia'],$_SESSION['_SBase'],$_SESSION['_Abono']);
-  $_SESSION['_INSS'] = calc_inss($_SESSION['_SBruto']);
-	$_SESSION['_SLiquido'] = calc_liquido($_SESSION['_SBruto'],$_SESSION['_INSS']);
-	
-	header('Location: ../view/folhapagamento.php');
-	
+
+  $_SFamilia = calc_familia($_NumFilhos);
+  $_Abono = calc_abono($_AnoNasc);
+  $_Idade = calc_idade($_AnoNasc);
+  $_SBruto = calc_bruto($_SFamilia, $_SBase, $_Abono);
+  $_INSS = calc_inss($_SBruto);
+  $_SLiquido = calc_liquido($_SBruto, $_INSS);
+
+  $_SESSION['dados'] = array('cpf'=>$_CPF, 'nomefuncionario'=>$_NomeFunc, 'nascimento'=>$_AnoNasc, 'salario'=>$_SBase, 'filhos'=>$_NumFilhos, 'salariofamilia'=>$_SFamilia, 'abono'=>$_Abono, 'idade'=>$_Idade, 'salariobruto'=>$_SBruto, 'inss'=>$_INSS, 'salarioliquido'=>$_SLiquido);
+
+  
+  header('Location: ../view/folhapagamento.php');
+  
 ?>
+  
+  
+
+
+ 
   
   
 
--- view/folhapagamento.php
+++ view/folhapagamento.php
@@ -11,15 +11,15 @@
 <body>
 	<h1> Cupom salarial </h1>
   <div id="mostrar_folha">
-		<p>CPF: <?php echo"$_SESSION['_cpf']"; ?> </p>
-		<p>Funcionário: <?php echo"$_SESSION['_Nome']"; ?> </p>
-		<p>Idade: <?php echo "$_SESSION['_Idade']"; ?> anos </p>
-		<p>Salário base: R$ <?php echo"$_SESSION['_SBase']";?> </p>
-		<p>Salário família: R$ <?php echo"$_SESSION['_SFamilia']"; ?></p>
-		<p>Abono: R$ <?php echo"$_SESSION['_Abono']"; ?></p>
-		<p>Salário bruto: R$ <?php echo"$_SESSION['_SBruto']"; ?></p>
-		<p>INSS: R$ <?php echo"$_SESSION['_INSS']"; ?></p>
-		<p>Salário liquido: R$ <?php echo"$_SESSION['_SLiquido']"; ?></p>
+		<p>CPF: <?php echo $_SESSION['dados']['cpf']; ?> </p>
+		<p>Funcionário: <?php echo $_SESSION['dados']['nomefuncionario']; ?> </p>
+		<p>Idade: <?php echo $_SESSION['dados']['idade']; ?> anos </p>
+		<p>Salário base: R$ <?php echo $_SESSION['dados']['salario'];?> </p>
+		<p>Salário família: R$ <?php echo $_SESSION['dados']['salariofamilia']; ?></p>
+		<p>Abono: R$ <?php echo $_SESSION['dados']['abono']; ?></p>
+		<p>Salário bruto: R$ <?php echo $_SESSION['dados']['salariobruto']; ?></p>
+		<p>INSS: R$ <?php echo $_SESSION['dados']['inss']; ?></p>
+		<p>Salário liquido: R$ <?php echo $_SESSION['dados']['salarioliquido']; ?></p>
   </div>
   <a href="../view/formulario.php"> Voltar </a>
 </body>
